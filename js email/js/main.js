0caf83533a9acccb620b728559d0f07b7d0a8140
Nicola-Campagna <campagnanicola379@gmail.com>
2023-01-30 13:51:24 +0100

    general refactoring

--- js email/js/main.js
+++ js email/js/main.js
@@ -1,5 +1,5 @@
 // TRACCIA
-// Mail
+// Mail:
 // Chiedi all'utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
@@ -7,38 +7,31 @@
 // PASSAGGI:
 
 // CHIEDERE all'utente la sua email:
-let insertMail = (prompt("inserire mail"));
+const insertMail = (prompt("inserire mail"));
 
 // ARRAY CON TUTTE LE MAIL CHE HANNO ACCESSO
-const userAllMails = [
+const mails = [
     "nicola@gmail.com",
-    "hello@gmail.com",
+    "ciao@gmail.com",
     "ciao123@gmail.com"
 ]
 
-// ciclo+array:
+let validMail = false;
 
-for (let i = 0; i < userAllMails.length; i++) {
-    const validMail = userAllMails[i];
+// Ciclo+Array
+for (let i = 0; i < mails.length; i++) {
+    const currentMail = mails[i];
     // console.log(currentMail);
-    // const currentMail = userAllMails[i];
-
-    // SE non combacia mai riprova
-    if (insertMail = validMail) {
-        console.log("accesso verificato");
-        alert("accesso verificato");
+    // SE nell'array una mail è uguale alla mail inserita (email da false diventa true)
+    if (currentMail == insertMail) {
+        validMail = true;
     }
-    // SE LA MAIL E' IDENTICA AD ALMENO UNA MAIL ALL'INTERNO DELL'ELENCO DI TUTTE LA MAIL , stampa messaggio e acceddi:
-    else {
-        let insertMail = (prompt("inserire mail"));
-        alert("accesso vietato");
-        console.log("accesso vietato");
-    }
-
 }
-
-
-
-
-
-
+// SE l'email non è corretta (quindi falsa) : nega l'accesso
+if (validMail == false) {
+    console.log("accesso vietato");
+}
+// Altrimenti: consenti l'accesso
+else {
+    console.log("accesso consentito");
+}
